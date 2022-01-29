0ad493466289f0d382705b3b0af5f53493d3421f
Scarlett Eller <rq6715@gmail.com>
2022-01-29 16:06:38 -0500

    commit tree a9a83b6ed8f0a597c548b61626c3e33686dd6255

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+
+
+function createEmployeeRecord(employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(employees) {
+    let employeeRecords = []
+    for (let i = 0; i < employees.length; i++) {
+        let record = createEmployeeRecord(employees[i])
+        employeeRecords.push(record)
+    }
+   
+    // employees.forEach((employee) => {
+    //     let record = createEmployeeRecord(employee)
+    //     employeeRecords.push(record)
+    // })
+    return employeeRecords;
+
+}
